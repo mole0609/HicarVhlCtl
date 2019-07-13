16893ca4b6ee35340a7e27ece24994a5f2b3e078
niyu <yuni@pateo.com.cn>
2019-07-13 17:39:55 +0800

    添加自定义pin控件

--- lib/utils/dialog_util.dart
+++ lib/utils/dialog_util.dart
@@ -12,7 +12,32 @@ class DialogWidget extends StatefulWidget {
   _DialogWidgetState createState() => _DialogWidgetState();
 }
 
-class _DialogWidgetState extends State<DialogWidget> {
+class _DialogWidgetState extends State<DialogWidget>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+    new AnimationController(vsync: this, duration: Duration(seconds: 1))
+      ..forward();
+//    _controller.addStatusListener(listener);
+    _animation = new Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.0,
+          0.1,
+          curve: Curves.easeIn,
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return CupertinoAlertDialog(
@@ -27,21 +52,21 @@ class _DialogWidgetState extends State<DialogWidget> {
           children: <Widget>[
             TextField(
               decoration: InputDecoration(
-                hintText: "请输入密码",
+                hintText: "请输入安全码",
                 hintStyle: TextStyle(
                   fontSize: 12,
                   letterSpacing: 1.0,
                 ),
                 filled: true,
-                fillColor: Colors.grey.shade50,
+                fillColor: Color(0x30FFFFFF),
                 contentPadding: EdgeInsets.only(left: 12, top: 15),
                 border: OutlineInputBorder(
                     borderSide:
-                        BorderSide(width: 0.0, style: BorderStyle.none)),
+                    BorderSide(width: 0.0, style: BorderStyle.none)),
                 counterText: '',
               ),
               maxLength: 6,
-              cursorWidth: 0.0,
+              showCursor: false,
               style: TextStyle(
                 letterSpacing: 22,
                 fontSize: 20,
